1e94c3fe3fb439a159c872e097552d59486ec8b6
Gustaf Matsson <gustafmatsson@gmail.com>
2018-08-21 14:49:51 +0200

    Added repository class and method addDomainTodb

---
+++ src/main/java/se/val18/miniprojekt/repo/Repository.java
@@ -0,0 +1,38 @@
+package se.val18.miniprojekt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+//by Gustaf Matsson
+//2018-08-21
+@Component
+public class Repository {
+
+    @Autowired
+    DataSource dataSource;
+
+    public void addDomainToDb(List<Domain> domain) throws SQLException {
+        try {
+            Connection conn = dataSource.getConnection();
+            Statement stmt = conn.createStatement();
+            for (Domain d : domain) {
+                int rs = stmt.executeUpdate("UPDATE Domain SET Name = \'" + d.name
+                + "\', URL= \'" + d.url + "\' WHERE ID= " + d.id);
+            }
+
+
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+
+    }
+}
+
