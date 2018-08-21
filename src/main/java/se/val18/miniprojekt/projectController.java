0f22efaefc94b77466bcc68233cc51ce163dec9d
Jennie Ahlberg <36049454+Jennieahlberg@users.noreply.github.com>
2018-08-21 13:45:35 +0200

    Add content in html, css files and methods in Controller class

--- src/main/java/se/val18/miniprojekt/projectController.java
+++ src/main/java/se/val18/miniprojekt/projectController.java
@@ -2,6 +2,8 @@ package se.val18.miniprojekt;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,5 +12,11 @@ public class projectController {
     @GetMapping("/")
     public ModelAndView index(){
         return new ModelAndView("index");
+    }
+
+    @PostMapping("/")
+    public ModelAndView index(@RequestParam String text){
+
+        return new ModelAndView("redirect:/index/");
     }
 }
