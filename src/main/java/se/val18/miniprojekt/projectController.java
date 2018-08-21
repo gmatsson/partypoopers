909baf051eb2ddaa4911ce84a9bd8dee772594bc
Jennie Ahlberg <36049454+Jennieahlberg@users.noreply.github.com>
2018-08-21 11:06:15 +0200

    Add index.html, style.css and projectController class

---
+++ src/main/java/se/val18/miniprojekt/projectController.java
@@ -0,0 +1,14 @@
+package se.val18.miniprojekt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class projectController {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
+}
