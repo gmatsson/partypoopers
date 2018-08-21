c84918fb609ca52ac9198af34ffaebe968601c79
Love Ahnström <love.ahnstrom@gmail.com>
2018-08-21 10:39:09 +0200

    Updated project structuere to include spring

---
+++ src/main/java/se/val18/miniprojekt/MiniprojektApplication.java
@@ -0,0 +1,12 @@
+package se.val18.miniprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprojektApplication.class, args);
+    }
+}
---
+++ src/test/java/se/val18/miniprojekt/MiniprojektApplicationTests.java
@@ -0,0 +1,16 @@
+package se.val18.miniprojekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiniprojektApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
